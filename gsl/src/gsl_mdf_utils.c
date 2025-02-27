1a1852619e1538e527afa02758b36bfee7a9e9b9
Venkatesh Mangalappali <quic_vmangala@quicinc.com>
2025-02-26 18:02:28 -0800

    gsl: fix handling dynamic PD service notifications
    - add support to handle satellite dynamic PD service UP/DOWN
    notifications as normal events for dynamic PD initialization and
    de-initialization and avoid crashing master proc.
    - register to root PD proxy service for receiving dynamic PD
    service UP/DOWN notifications and manage the subsystem state
    properly when handling dynamic PD crash.
    
    Change-Id: Ie20a819b252c304ac7e51f79cc17a44a384ee011
    (cherry picked from commit 636929c4d2942116bd260c5b78479f8e1b070c44)

--- gsl/src/gsl_mdf_utils.c
+++ gsl/src/gsl_mdf_utils.c
@@ -60,22 +60,33 @@ struct gsl_mdf_info {
 	uint32_t num_procs;
 	/* susbystems domain type information */
 	struct proc_domain_type *pd_info;
-} _gsl_glb_mdf_info = {0, NULL, 0, 0, NULL};
+	/* dynamic pd reference count */
+	uint32_t pd_init_ref_cnt[AR_SUB_SYS_ID_LAST];
+	/* dynamic pd deinit pending used to skip PD DOWN treated as crash */
+	bool_t pd_deinit_pending[AR_SUB_SYS_ID_LAST];
+
+} _gsl_glb_mdf_info = {0, NULL, 0, 0, NULL, {0}, {FALSE}};
 
 static bool_t is_initialized = FALSE;
-/* dynamic pd reference count */
-static uint32_t pd_init_ref_cnt[AR_SUB_SYS_ID_LAST] = {0};
 
-static bool_t gsl_mdf_utils_is_dynamic_pd(uint32_t sys_id)
+bool_t gsl_mdf_utils_is_dynamic_pd(uint32_t proc_id)
 {
 	uint32_t i = 0;
 
 	for (i = 0; i < _gsl_glb_mdf_info.num_procs; ++i) {
-		if (_gsl_glb_mdf_info.pd_info[i].proc_id == sys_id &&
+		if (_gsl_glb_mdf_info.pd_info[i].proc_id == proc_id &&
 			_gsl_glb_mdf_info.pd_info[i].proc_type == DYNAMIC_PD)
 				return TRUE;
 	}
 	return FALSE;
+}
+
+bool_t gsl_mdf_utils_is_dynamic_pd_deinit_pending(uint32_t proc_id)
+{
+	if (proc_id < AR_SUB_SYS_ID_FIRST || proc_id > AR_SUB_SYS_ID_LAST)
+		return FALSE;
+
+	return _gsl_glb_mdf_info.pd_deinit_pending[proc_id];
 }
 
 uint32_t gsl_mdf_utils_query_graph_ss_mask(uint32_t *sg_id_list,
@@ -538,17 +549,18 @@ int32_t gsl_mdf_utils_register_dynamic_pd(uint32_t ss_mask,
 				goto next;
 
 			*dyn_ss_mask |= GSL_GET_SPF_SS_MASK(sys_id);
-			if (pd_init_ref_cnt[sys_id] == 0) {
+			if (_gsl_glb_mdf_info.pd_init_ref_cnt[sys_id] == 0) {
 				GSL_DBG("initialize dynamic pd %d", sys_id);
 				rc = ar_osal_dyn_pd_init(sys_id);
 				if (rc) {
 					GSL_ERR("ar_osal_dyn_pd_init failed %d", rc);
 					goto de_init_pd;
 				}
-				++pd_init_ref_cnt[sys_id];
+				++_gsl_glb_mdf_info.pd_init_ref_cnt[sys_id];
 				tmp_pd_list[pd_cnt++] = sys_id;
 				sm = GSL_GET_SPF_SS_MASK(sys_id) |
 					GSL_GET_SPF_SS_MASK(master_proc_id);
+				GSL_DBG("Allocate memory ss_mask 0x%x", sm);
 				rc = gsl_mdf_utils_shmem_alloc(sm, master_proc_id);
 				if (rc != AR_EOK) {
 					GSL_ERR("failed to alloc loaned shmem %d", rc);
@@ -563,7 +575,7 @@ int32_t gsl_mdf_utils_register_dynamic_pd(uint32_t ss_mask,
 					goto de_init_pd;
 				}
 			} else {
-				++pd_init_ref_cnt[sys_id];
+				++_gsl_glb_mdf_info.pd_init_ref_cnt[sys_id];
 				tmp_pd_list[pd_cnt++] = sys_id;
 			}
 		}
@@ -576,9 +588,9 @@ int32_t gsl_mdf_utils_register_dynamic_pd(uint32_t ss_mask,
 de_init_pd:
 	while (pd_cnt > 0) {
 		--pd_cnt;
-		if (pd_init_ref_cnt[tmp_pd_list[pd_cnt]] == 1)
+		if (_gsl_glb_mdf_info.pd_init_ref_cnt[tmp_pd_list[pd_cnt]] == 1)
 			ar_osal_dyn_pd_deinit(tmp_pd_list[pd_cnt]);
-		--pd_init_ref_cnt[tmp_pd_list[pd_cnt]];
+		--_gsl_glb_mdf_info.pd_init_ref_cnt[tmp_pd_list[pd_cnt]];
 	}
 	return rc;
 }
@@ -600,17 +612,20 @@ int32_t gsl_mdf_utils_deregister_dynamic_pd(uint32_t ss_mask,
 				!gsl_mdf_utils_is_dynamic_pd(sys_id)) {
 				goto next;
 			}
-			if (pd_init_ref_cnt[sys_id] == 1) {
+			if (_gsl_glb_mdf_info.pd_init_ref_cnt[sys_id] == 1) {
 				sm = GSL_GET_SPF_SS_MASK(sys_id) |
 					GSL_GET_SPF_SS_MASK(master_proc_id);
+				GSL_DBG("Free memory ss_mask 0x%x", sm);
 				rc = gsl_mdf_utils_shmem_free(sm);
 				if (rc != AR_EOK)
 					GSL_ERR("failed to free loaned shmem %d", rc);
 				GSL_DBG("deinitialize dynamic pd %d", sys_id);
+				_gsl_glb_mdf_info.pd_deinit_pending[sys_id] = TRUE;
 				ar_osal_dyn_pd_deinit(sys_id);
+				_gsl_glb_mdf_info.pd_deinit_pending[sys_id] = FALSE;
 			}
-			if (pd_init_ref_cnt[sys_id] > 0)
-				--pd_init_ref_cnt[sys_id];
+			if (_gsl_glb_mdf_info.pd_init_ref_cnt[sys_id] > 0)
+				--_gsl_glb_mdf_info.pd_init_ref_cnt[sys_id];
 		}
 	next:
 		++sys_id;
